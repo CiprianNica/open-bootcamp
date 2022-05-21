98360516fbcf6f9c8aea48a9fb54252c2b15f8cb
CiprianNica <ciprian.nica77@gmail.com>
2022-05-22 00:33:17 +0200

    tema3.1

---
+++ src/tema31.java
@@ -0,0 +1,10 @@
+public class tema31{
+    public static void main (String[] args){
+        int resultado = 0;
+        resultado = suma(4, 5, 7);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c ){
+        return a+b+c;
+    }
+}
