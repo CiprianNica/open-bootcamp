58ad9dfeaa04e43a8db2c66908af39e1df7139cf
CiprianNica <ciprian.nica77@gmail.com>
2022-05-28 18:52:48 +0200

    tema8 final

---
+++ src/tema8.java
@@ -0,0 +1,50 @@
+public class tema8 {
+    public static void main(String[] args){
+        Persona persona = new Persona();
+
+        persona.setNombre("Ion");
+        System.out.println(persona.getNombre());
+
+        persona.setEdad(42);
+        System.out.println(persona.getEdad());
+
+        persona.setTelefono(12345667);
+        System.out.println(persona.getTelefono());
+    }
+}
+
+/*
+   Para practicar la encapsulación
+   Crear clase Persona.
+        Crear variables las privadas edad, nombre
+        y telefono de la clase Persona.
+        Crear gets y sets de cada propiedad.
+        Crear un objeto persona en el main.
+        Utiliza los gets y sets para darle valores a
+        las propiedades edad, nombre y telefono, por último
+        muéstralas por consola.
+*/
+class Persona{
+    private int edad, telefono;
+    private String nombre;
+// setter
+    public void  setEdad(int edadValor){
+        this.edad = edadValor;
+    }
+    public void setNombre(String nombreValor){
+        this.nombre = nombreValor;
+    }
+    public void setTelefono( int telefonoValor){
+        this.telefono = telefonoValor;
+    }
+// getter
+    public int getEdad(){
+        return this.edad;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+}
