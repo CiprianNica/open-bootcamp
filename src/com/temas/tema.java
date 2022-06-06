ebea125f8a6ecfb5daf4e323a0423e7ba01d254a
CiprianNica <ciprian.nica77@gmail.com>
2022-06-06 21:03:02 +0200

    sesion 2 tema final

---
+++ src/com/temas/tema.java
@@ -0,0 +1,31 @@
+package com.temas;
+
+public class tema {
+    public static void main(String[] args) {
+        // numericos enteros
+        byte numeroByte = 3;
+        short numeroShort = 4;
+        int numeroInt = 5;
+        long numeroLong = 6;
+        System.out.println("byte: "+numeroByte);
+        System.out.println("short: "+numeroShort);
+        System.out.println("int: "+numeroInt);
+        System.out.println("long: "+numeroLong);
+        //numeros decimale
+        float numeroFloat = 4.6f;
+        double numeroDouble = 5.6d;
+        System.out.println("float: "+numeroFloat);
+        System.out.println("double: "+numeroDouble);
+
+        // datos boolean
+        boolean varBoolean1 = false;
+        boolean varBoolean2 = true;
+        System.out.println("boolean1: "+varBoolean1);
+        System.out.println("boolean2: "+varBoolean2);
+        // textos
+        char txtChar = 'w';
+        String txtString = "Lprem ipsum sdfer weat.";
+        System.out.println("texto char: "+txtChar);
+        System.out.println("texto string: "+txtString);
+    }
+}
